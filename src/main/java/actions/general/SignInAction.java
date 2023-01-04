f556618f89f04d064dabc6a3b4920b1676023448
mistaferry <irynka.huryn@gmail.com>
2023-01-04 18:41:14 +0100

    from the login page to the page with a list of users!

--- src/main/java/actions/general/SignInAction.java
+++ src/main/java/actions/general/SignInAction.java
@@ -20,30 +20,22 @@ public class SignInAction implements Action {
     @Override
     public String execute(HttpServletRequest request) throws ServletException, IOException, ServiceException {
         String path = null;
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
         try{
+            UserDTO user = generalService.signIn(login, password);
+            request.setAttribute("user", user);
+            request.setAttribute("role", user.getRoleId());
             List<UserDTO> cruiseDTOList = generalService.viewCatalog();
             request.getSession().setAttribute("cruises", cruiseDTOList);
-            path = "/pages/test.jsp";
+            switch (user.getRoleId()) {
+                case 1 -> path = "/pages/test.jsp";
+                case 2 -> path = "/pages/test.jsp";
+                case 3 -> path = "/pages/test.jsp";
+            }
         } catch (ServiceException e) {
-            e.printStackTrace();
-            path = "/pages/errorPage.jsp";
+            return "/pages/errorPage.jsp";
         }
         return path;
-//        String path = null;
-//        String login = request.getParameter("login");
-//        String password = request.getParameter("password");
-//        try{
-//            UserDTO user = generalService.signIn(login, password);
-//            request.setAttribute("user", user);
-//            request.setAttribute("role", user.getRoleId());
-//            switch (user.getRoleId()) {
-//                case 1 -> path = "/pages/test.jsp";
-//                case 2 -> path = "/pages/test.jsp";
-//                case 3 -> path = "/pages/test.jsp";
-//            }
-//        } catch (ServiceException e) {
-//            return "/pages/errorPage.jsp";
-//        }
-//        return path;
     }
 }
